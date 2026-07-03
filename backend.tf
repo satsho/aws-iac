642144f237b5932a627fca615bc0d8802a62eadd
satsho <emeth.meth0307@gmail.com>
2026-07-03 14:49:35 +0000

    20260703-002

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "tfstate-123456789012-apne1"
+    bucket       = "terraform-state-repo-satsho"
     key          = "iam/terraform.tfstate"
     region       = "ap-northeast-1"
     use_lockfile = true          # Terraform 1.10+ の S3 ネイティブロック
